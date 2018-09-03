cb3855e39408c833b19f3dc89def21c5563dde0e
Ryan Brellenthin <rbrellen@gmail.com>
2018-09-03 04:08:36 -0400

    Limit to single input directory

--- R/create_output.R
+++ R/create_output.R
@@ -9,8 +9,8 @@
 #' \dontrun{
 #' course_ratings <- create_output('~/courses/')
 #' }
-create_output <- function(input_directories) {
-  list_of_input_files <- list.files(input_directories)
+create_output <- function(input_directory) {
+  list_of_input_files <- paste0(input_directory, list.files(input_directory))
 
   result <- dplyr::bind_rows(lapply(list_of_input_files, create_row)) %>%
     purrr::set_names(c("Course Number", "Faculty Name(s)", "Semester",
